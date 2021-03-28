76f10097b45626c40a753dbc63295c8f1727f3c5
Rice <a00279376@student.ait.ie>
2021-03-28 17:34:59 +0200

    changes

--- src/main/java/edu/ait/games/repositories/FollowerRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.ait.games.repositories;
-
-import edu.ait.games.dto.Follower;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FollowerRepository extends JpaRepository<Follower, Integer> {
-}
--- src/main/java/edu/ait/games/repositories/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.ait.games.repositories;
-
-import edu.ait.games.dto.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
